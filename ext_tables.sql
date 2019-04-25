d59187db1736b9cfb900b3813f4f085d9d29028e
tgriessbach <griessbach@portrino.de>
2019-04-25 10:07:07 +0200

    updates sql to prevent key length error

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,7 +2,7 @@
 # Table structure for table 'tx_pxdbsequencer_sequence'
 #
 CREATE TABLE tx_pxdbsequencer_sequence (
-	table_name varchar(255) DEFAULT '' NOT NULL,
+	table_name varchar(190) DEFAULT '' NOT NULL,
 	current int(30) DEFAULT '0' NOT NULL,
 	offset int(30) DEFAULT '1' NOT NULL,
 	timestamp int(30) DEFAULT '0' NOT NULL,
